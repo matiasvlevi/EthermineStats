34211cd4e0ab95cc97228f68e3a46f4663c5ad06
matiasvlevi <matiasvlevi@gmail.com>
2021-03-15 18:39:18 -0400

    getstat

--- getstat.js
+++ getstat.js
@@ -1,10 +1,15 @@
 
 const fs = require('fs');
-//const Board = require('./methods/board.js');
+const Ethermine = require('ethermine-api');
+const ethermine = new Ethermine();
 
-const data = JSON.parse(fs.readFileSync(path,'utf-8'));
+ethermine.getMinerPayouts(miner, function(err, pay) {
+    let path = './data/stat_payout_'+pay.data.length+'.json';
+    const data = JSON.parse(fs.readFileSync(path,'utf-8'));
 
-console.log("Update "+data.totalchecks + ":");
-for (let worker of data.workers) {
-    console.log("	- "+worker.name +": "+worker.percent);
-}
+    console.log("Update "+data.totalchecks + ":");
+    for (let worker of data.workers) {
+        console.log("	- "+worker.name +": "+worker.percent);
+    }
+
+})
